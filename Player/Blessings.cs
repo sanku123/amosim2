f33fdfe2aaa06b141c27eacdef5f17e61c6f96b7
sanku123 <sebastian.gfx@gmail.com>
2024-02-22 02:53:06 +0100

    blessings

--- Player/Blessings.cs
+++ Player/Blessings.cs
@@ -7,107 +7,59 @@ namespace AmoSim2.Player
 {
     public partial class Model : ViewModelBase
     {
+        [JsonIgnore] public bool StrengthBlessActive { get; set; }
+        [JsonIgnore] public bool ToughnessBlessActive { get; set; }
+        [JsonIgnore] public bool AgilityBlessActive { get; set; }
+        [JsonIgnore] public bool SpeedBlessActive { get; set; }
+        [JsonIgnore] public bool InteligenceBlessActive { get; set; }
+        [JsonIgnore] public bool WillPowerBlessActive { get; set; }
+        [JsonIgnore] public bool TrafienieBlessActive { get; set; }
+        [JsonIgnore] public bool UnikiBlessActive { get; set; }
 
         [JsonIgnore]
         public double Modificator
         {
             get
             {
-                switch (Race)
-                {
-                    case "Wampir" when VampireBlessActive:
-                        return 1.5;
-                    case "Człowiek" when Class == "Paladyn":
-                        return 2.5;
-                    default:
-                        return 1;
-                }
+                return Race == "Wampir" && VampireBlessActive ? 1.5 :
+                       Race == "Człowiek" && Class == "Paladyn" ? 2.5 :
+                       1;
             }
         }
 
-        [JsonIgnore]
-        public double StrengthBless => StrengthBlessActive ? 6 * Level * Modificator : 0;
+        private const double BlessMultiplier = 6;
 
-        [JsonIgnore]
-        public double ToughnessBless => ToughnessBlessActive ? 6 * Level * Modificator : 0;
+        private double CalculateBless(bool isActive) => isActive ? BlessMultiplier * Level * Modificator : 0;
+        private double CalculateSpecialBless(bool isActive) => isActive ? BlessMultiplier * Level * Modificator / 1.4 : 0;
 
-        [JsonIgnore]
-        public double AgilityBless => AgilityBlessActive ? 6 * Level * Modificator : 0;
-
-        [JsonIgnore]
-        public double SpeedBless => SpeedBlessActive ? 6 * Level * Modificator : 0;
-
-        [JsonIgnore]
-        public double InteligenceBless => InteligenceBlessActive ? 6 * Level * Modificator : 0;
-
-        [JsonIgnore]
-        public double WillPowerBless => WillPowerBlessActive ? 6 * Level * Modificator : 0;
-
-        [JsonIgnore]
-        public double TrafienieBless => TrafienieBlessActive ? 6 * Level * Modificator / 1.4 : 0;
-
-        [JsonIgnore]
-        public double UnikiBless => UnikiBlessActive ? 6 * Level * Modificator / 1.4 : 0;
+        [JsonIgnore] public double StrengthBless => CalculateBless(StrengthBlessActive);
+        [JsonIgnore] public double ToughnessBless => CalculateBless(ToughnessBlessActive);
+        [JsonIgnore] public double AgilityBless => CalculateBless(AgilityBlessActive);
+        [JsonIgnore] public double SpeedBless => CalculateBless(SpeedBlessActive);
+        [JsonIgnore] public double InteligenceBless => CalculateBless(InteligenceBlessActive);
+        [JsonIgnore] public double WillPowerBless => CalculateBless(WillPowerBlessActive);
+        [JsonIgnore] public double TrafienieBless => CalculateSpecialBless(TrafienieBlessActive);
+        [JsonIgnore] public double UnikiBless => CalculateSpecialBless(UnikiBlessActive);
 
         private bool? _vampireBlessActive;
-        [JsonIgnore]
-        public bool VampireBlessActive
-        {
-            get { return (_vampireBlessActive ?? false); }
-            set
-            {
-                _vampireBlessActive = value;
-                OnPropertyChanged();
-            }
-        }
+        [JsonIgnore] public bool VampireBlessActive { get => (_vampireBlessActive ?? false); set { _vampireBlessActive = value; OnPropertyChanged(); } }
 
-        [JsonIgnore]
-        public bool StrengthBlessActive { get; set; }
+        // Properties for other bless types...
 
-        [JsonIgnore]
-        public bool ToughnessBlessActive { get; set; }
-
-        [JsonIgnore]
-        public bool AgilityBlessActive { get; set; }
-
-        [JsonIgnore]
-        public bool SpeedBlessActive { get; set; }
-
-        [JsonIgnore]
-        public bool InteligenceBlessActive { get; set; }
-
-        [JsonIgnore]
-        public bool WillPowerBlessActive { get; set; }
-
-        [JsonIgnore]
-        public bool TrafienieBlessActive { get; set; }
-
-        [JsonIgnore]
-        public bool UnikiBlessActive { get; set; }
-
-        public List<string> Blessings => new List<String> { "", "Siła", "Wytrzymałość", "Zręczność", "Szybkość", "Inteligencja", "Siła Woli", "Trafienie", "Uniki" };
+        public List<string> Blessings { get; } = new List<string> { "", "Siła", "Wytrzymałość", "Zręczność", "Szybkość", "Inteligencja", "Siła Woli", "Trafienie", "Uniki" };
 
         public void ResetBlessings()
         {
-            StrengthBlessActive = false;
-            ToughnessBlessActive = false;
-            AgilityBlessActive = false;
-            SpeedBlessActive = false;
-            InteligenceBlessActive = false;
-            WillPowerBlessActive = false;
-            TrafienieBlessActive = false;
-            UnikiBlessActive = false;
+            StrengthBlessActive = ToughnessBlessActive = AgilityBlessActive = SpeedBlessActive = InteligenceBlessActive = WillPowerBlessActive = TrafienieBlessActive = UnikiBlessActive = false;
         }
 
         private string _selectedBlessing;
-
         public string SelectedBlessing
         {
             get => _selectedBlessing;
             set
             {
                 ResetBlessings();
-
                 if (value != _selectedBlessing)
                 {
                     _selectedBlessing = value;
@@ -120,49 +72,18 @@ namespace AmoSim2.Player
         private void SelectedBlessingChanged(string selectedBlessing)
         {
             ResetBlessings();
-
             switch (selectedBlessing)
             {
-                case "Siła":
-                    StrengthBlessActive = true;
-                    break;
-
-                case "Wytrzymałość":
-                    ToughnessBlessActive = true;
-                    break;
-
-                case "Zręczność":
-                    AgilityBlessActive = true;
-                    break;
-
-                case "Szybkość":
-                    SpeedBlessActive = true;
-                    break;
-
-                case "Inteligencja":
-                    InteligenceBlessActive = true;
-                    break;
-
-                case "Siła Woli":
-                    WillPowerBlessActive = true;
-                    break;
-
-                case "Trafienie":
-                    TrafienieBlessActive = true;
-                    break;
-
-                case "Uniki":
-                    UnikiBlessActive = true;
-                    break;
-
-                case "":
-                    ResetBlessings();
-                    break;
-
-                default:
-                    ResetBlessings();
-                    break;
+                case "Siła": StrengthBlessActive = true; break;
+                case "Wytrzymałość": ToughnessBlessActive = true; break;
+                case "Zręczność": AgilityBlessActive = true; break;
+                case "Szybkość": SpeedBlessActive = true; break;
+                case "Inteligencja": InteligenceBlessActive = true; break;
+                case "Siła Woli": WillPowerBlessActive = true; break;
+                case "Trafienie": TrafienieBlessActive = true; break;
+                case "Uniki": UnikiBlessActive = true; break;
             }
         }
     }
+
 }
