469d90c80106cad84a106784d592e731da8bf69c
sanku123 <sebastian.gfx@gmail.com>
2024-02-22 03:58:51 +0100

    inicjatywa

--- Simulation.cs
+++ Simulation.cs
@@ -20,28 +20,6 @@ namespace AmoSim2.Player
         [JsonIgnore]
         public PlayerViewModel PlayerViewModel => ServiceLocator.Current.GetInstance<PlayerViewModel>();
 
-
-        public double PlayerInicjatywaBase => Math.Round(PlayerViewModel.Player.BattleSpeed / EnemyViewModel.Enemy.BattleSpeed, 3);
-
-        public double EnemyInicjatywaBase => Math.Round(EnemyViewModel.Enemy.BattleSpeed / PlayerViewModel.Player.BattleSpeed, 3);
-
-
-        [JsonIgnore]
-        public double PlayerInicjatywa => GetInicjatywa(PlayerInicjatywaBase);
-
-        [JsonIgnore]
-        public double EnemyInicjatywa => GetInicjatywa(EnemyInicjatywaBase);
-
-        private double GetInicjatywa(double baseValue)
-        {
-            if (baseValue >= 4.2) return 5;
-            else if (baseValue >= 3.2) return 4;
-            else if (baseValue >= 2.2) return 3;
-            else if (baseValue >= 1.15) return 2;
-            else return 1;
-        }
-
-
         [JsonIgnore]
         public double PlayerHitChance => CalculateHitChance(PlayerViewModel.Player.HitAbility, EnemyViewModel.Enemy.EvasionFull);
 
@@ -53,5 +31,22 @@ namespace AmoSim2.Player
             double val = Math.Round(((Math.Log10(attackerAbility + 200) - Math.Log10(defenderEvasion + 200)) * 500) + 50, 2);
             return Math.Min(val, 98); // Ensure the value does not exceed 98
         }
+        public double PlayerInicjatywaBase => Math.Round(PlayerViewModel.Player.BattleSpeed / EnemyViewModel.Enemy.BattleSpeed, 3);
+
+        public double EnemyInicjatywaBase => Math.Round(EnemyViewModel.Enemy.BattleSpeed / PlayerViewModel.Player.BattleSpeed, 3);
+
+        [JsonIgnore]
+        public double PlayerInicjatywa => GetInicjatywa(PlayerInicjatywaBase);
+
+        [JsonIgnore]
+        public double EnemyInicjatywa => GetInicjatywa(EnemyInicjatywaBase);
+
+        private double GetInicjatywa(double baseValue)
+        {
+            return baseValue >= 4.2 ? 5 :
+                   baseValue >= 3.2 ? 4 :
+                   baseValue >= 2.2 ? 3 :
+                   baseValue >= 1.15 ? 2 : 1;
+        } 
     }
 }
